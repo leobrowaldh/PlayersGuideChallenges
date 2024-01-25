d6b61d06f40d6b7e9d046559847e2b20d4d9073d
leobrowaldh <leobrowaldh@gmail.com>
2024-01-25 17:16:35 +0100

    L38 done

---
+++ L38_TheLambdaSieve/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace L38_TheLambdaSieve;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("select your evaluation method");
+        Console.WriteLine("1. The number is even");
+        Console.WriteLine("2. The number is positive");
+        Console.WriteLine("3. The number is multiple of 10");
+        int selectedNumber = int.Parse(Console.ReadLine());
+        Sieve sieve = null;
+        switch (selectedNumber)
+        {
+            case 1:
+                {
+                    sieve = new Sieve(n => n % 2 == 0);
+                    break;
+                }
+            case 2:
+                {
+                    sieve = new Sieve(n => n > 0);
+                    break;
+                }
+            case 3:
+                {
+                    sieve = new Sieve(n => n % 10 == 0);
+                    break;
+                }
+        }
+
+        string selection = "";
+        while (selection != "q")
+        {
+            Console.WriteLine("Enter a number: ");
+            selection = Console.ReadLine();
+            if (int.TryParse(selection, out selectedNumber))
+            {
+                Console.WriteLine(sieve.IsGood(selectedNumber));
+            }
+        }
+    }
+}
+
+public class Sieve(Func<int, bool> judgingMethod)
+{
+    public Func<int, bool> IsGood { get; set; } = judgingMethod;
+}
